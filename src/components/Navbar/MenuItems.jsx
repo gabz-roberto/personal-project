a726068302c01b2b526f79cea1fd479a3b675249
Gabriel Roberto <gabrielrobertoal@gmail.com>
2020-09-19 12:13:30 -0300

    menu

---
+++ src/components/Navbar/MenuItems.jsx
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Início',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sobre',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Projetos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Artigos',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contato',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
